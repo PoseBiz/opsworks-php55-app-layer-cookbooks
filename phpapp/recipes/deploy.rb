5b4e50571e83613ae203fa186af98ec443570e91
Thomas Le Blan <thomas@littleblackbag.com>
2013-11-27 19:46:41 -0800

    trying other path

--- phpapp/recipes/deploy.rb
+++ phpapp/recipes/deploy.rb
@@ -59,8 +59,9 @@ node[:deploy].each do |app_name, deploy|
     user "#{node['phpapp']['deploy']['user']}"
     cwd "/var/#{app_name}"
     code <<-EOH
+    ll
     curl -s https://getcomposer.org/installer | php
-    php composer.phar install --prefer-source --no-interaction -v --working-dir="/var/#{app_name}/portal/"
+    php composer.phar install --prefer-source --no-interaction -v --working-dir="/var/pose_php/portal/"
     EOH
   end
   
